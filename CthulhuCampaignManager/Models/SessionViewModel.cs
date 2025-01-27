8f930e7ab3249c421da0f75e80c1d7c165b7bf43
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-27 11:30:42 +0100

    Add SessionViewModel with basic properties

---
+++ CthulhuCampaignManager/Models/SessionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CthulhuCampaignManager.Models
+{
+    public class SessionViewModel
+    {
+        public int Id { get; set; }
+        public int CampaignId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Description { get; set; }
+    }
+}

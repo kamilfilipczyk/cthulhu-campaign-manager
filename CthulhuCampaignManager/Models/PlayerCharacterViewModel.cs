1c9b201043d81a4058de8b3df174e39e40d6dcf1
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-27 11:26:01 +0100

    Add PlayerCharacterViewModel with basic properties

---
+++ CthulhuCampaignManager/Models/PlayerCharacterViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CthulhuCampaignManager.Models
+{
+    public class PlayerCharacterViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ProfessionId { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int CampaignId { get; set; }
+        public int PlayerId { get; set; }
+    }
+}

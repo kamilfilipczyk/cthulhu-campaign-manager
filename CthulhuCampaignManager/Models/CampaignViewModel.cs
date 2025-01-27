ccfc41dcf667895928a29388de1d23875a842250
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-27 11:28:26 +0100

    Add CampaignViewModel with basic properties

---
+++ CthulhuCampaignManager/Models/CampaignViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CthulhuCampaignManager.Models
+{
+    public class CampaignViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartedAt { get; set; }
+        public string Status { get; set; }
+    }
+}

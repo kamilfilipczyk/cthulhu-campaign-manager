ad67b7606076ef077dabb8882039176b3fefb2d2
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-28 11:15:31 +0100

    Create Campaigns Controller with according View

---
+++ CthulhuCampaignManager/Controllers/CampaignsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CthulhuCampaignManager.Controllers
+{
+    public class CampaignsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

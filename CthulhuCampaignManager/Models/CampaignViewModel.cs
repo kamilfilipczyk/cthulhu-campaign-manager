b982400fd6304b7c61c95155899a093bb9d769cf
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-28 10:04:20 +0100

    Refactor CampaignViewModel

--- CthulhuCampaignManager/Models/CampaignViewModel.cs
+++ CthulhuCampaignManager/Models/CampaignViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CthulhuCampaignManager.Data;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace CthulhuCampaignManager.Models
@@ -7,13 +8,16 @@ namespace CthulhuCampaignManager.Models
     {
         [HiddenInput]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Title is missing!")]
         [StringLength(100, ErrorMessage = "Title is too long!")]
         public string Title { get; set; }
+
         [Required(ErrorMessage = "A campaign must have a description!")]
         [StringLength(500, ErrorMessage = "Description is too long!")]
         public string Description { get; set; }
-        public DateTime StartedAt { get; set; } = DateTime.UtcNow;
-        public string Status { get; set; } = "Ongoing";
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        public Status Status { get; set; } = Status.Ongoing;
+
     }
 }

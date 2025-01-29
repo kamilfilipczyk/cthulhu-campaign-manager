3eda692c34d42353de3076c5ebe835fa0b69c864
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-29 10:11:27 +0100

    Add DbContext to CampaignsController

--- CthulhuCampaignManager/Controllers/CampaignsController.cs
+++ CthulhuCampaignManager/Controllers/CampaignsController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CthulhuCampaignManager.Domain.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CthulhuCampaignManager.Controllers
 {
     public class CampaignsController : Controller
     {
+        private readonly ApplicationDbContext _dbcontext;
+        public CampaignsController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
         public IActionResult Index()
         {
             return View();
